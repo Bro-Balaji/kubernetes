6675c1e84be6530659a5402a7b5833161d8260e0
Bro-Balaji <bm27111999@gmail.com>
2024-08-31 16:32:26 +0530

    addded a new file

---
+++ bro.sh
@@ -0,0 +1,10 @@
+
+for i in `echo {1..10}`;do echo "I hate git bro seriously..";done
+
+
+
+first add the changes to the staging area and commit it to the local repository
+git add <filename>
+git commit -m "Pushed a new file" - to push to the local repository
+git mv 
+git rm
